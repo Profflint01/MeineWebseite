0f0809a46e270e4c2b656c61e3afb791fc2619a2
Profflint01 <profflint01@gmail.com>
2025-01-28 17:13:50 +0100

    Add files via upload

---
+++ Webseite V1.0.3/JavaSkript/java.js
@@ -0,0 +1,16 @@
+function EnableDarkTheme() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+var typed = new Typed(".multitext", {
+    strings: ["Label", "Thanks for visiting my website!", "I am profflint, a Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+    cursorChar: "|",
+    cursor: true,     
+});
+
+//document.getElementById("email").autocomplete = "on";
